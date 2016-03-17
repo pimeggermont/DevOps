e28a6d401efa8d38453c79f695300a899ffef2a8
janosh <janosh.1@hotmail.com>
2016-03-17 11:56:39 +0100

    server code toegevoegd voor write naar json

--- js/expressServer.js
+++ js/expressServer.js
@@ -33,6 +33,23 @@ app.get("/aanwezigen", function(req, res) {
 	res.json(aanwezigen);
 });
 
+var fs = require('fs');
+
+var myData = {      //variable waar de data van client inkomt
+  name:'test',
+  version:'1.0'
+}
+
+var outputFilename = 'http://localhost:3000/aanwezigen.json';
+
+fs.writeFile(outputFilename, JSON.stringify(myData, null, 4), function(err) {
+    if(err) {
+      console.log(err);
+    } else {
+      console.log("JSON saved to " + outputFilename);
+    }
+});
+
 var port = Number(process.env.PORT || 3000);
 
 app.listen(port);
