b0f19bb9a5d822db0ecadd2b4034a146d5ed9eb0
Jordi Lambrechts <lambrechtsjordi@hotmail.be>
2016-03-17 12:07:40 +0100

    Added missing code

--- js/expressServer.js
+++ js/expressServer.js
@@ -32,6 +32,23 @@ request({url: url, json: true}, function (error, response, body) {
     };
 });
 
+var fs = require('fs');
+
+var myData = {      //variable waar de data van client inkomt
+  name:'test',
+  version:'1.0'
+}
+
+var outputFilename = 'http://localhost:3000/aanwezigen.json';
+
+fs.writeFile(outputFilename, JSON.stringify(myData, null, 4), function(err) {
+    if(err) {
+      console.log(err);
+    } else {
+      console.log("JSON saved to " + outputFilename);
+    }
+});
+
 app.get("/aanwezigen", function(req, res) {
 	res.json(aanwezigen);
 });
